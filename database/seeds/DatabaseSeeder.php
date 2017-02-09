5be04dc258c526587517f6d7acb52360e9e963e3
super <qanhdk@gmail.com>
2017-02-09 21:19:33 +0700

    seed subjects

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         $this->call(SubjectSeeder::class);
-        $this->call(TimetableSeeder::class);
+         $this->call(SubjectSeeder::class);
+//        $this->call(TimetableSeeder::class);
     }
 }
